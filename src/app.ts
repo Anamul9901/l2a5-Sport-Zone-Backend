0a36d02215844082f98c018a97b56708e709aacf
Anamul Haque <anamulhaque9901@gmail.com>
2024-06-11 23:09:57 +0600

    assignment-3 server setup

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import cors from 'cors'
+import express, { Application } from 'express'
+const app: Application = express()
+
+//parsers
+app.use(express.json())
+app.use(cors())
+
+export default app
