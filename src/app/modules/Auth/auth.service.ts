88f0740904790d275baf357b2499f891f886ff83
Anamul Haque <anamulhaque9901@gmail.com>
2024-06-12 21:18:49 +0600

    check login email and password in auth

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,10 +1,29 @@
-import { TLoginUser } from "./auth.interface";
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
+import { TLoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
   console.log(payload);
-  return {};
+
+  //checking if ther user is exist
+  const isUserExists = await User.findOne({ email: payload?.email });
+  console.log(isUserExists);
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Thsis user is not found !');
+  }
+
+  // checking if ther password is correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    isUserExists?.password
+  );
+  console.log(isPasswordMatched);
+
+  return { isUserExists };
 };
 
 export const AuthServices = {
-    loginUser
-}
+  loginUser,
+};
