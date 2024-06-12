cc26578209c80176cd807a1edfdf94e62b03c0fa
Anamul Haque <anamulhaque9901@gmail.com>
2024-06-12 14:49:25 +0600

    facility booking schedule validate

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,10 +1,42 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
 import { TBooking } from './booking.interface';
 import { Booking } from './booking.model';
 
 const createBookingIntoDB = async (payload: TBooking) => {
+  const { date, startTime, endTime, user, facility, payableAmount } = payload;
 
   //get the schedules of the faculties
+  const assignedSchedules = await Booking.find({
+    facility,
+    date: { $in: date },
+  }).select('date startTime endTime');
+  console.log('assignedSchedules:', assignedSchedules);
 
+  const newSchedule = {
+    date,
+    startTime,
+    endTime,
+  };
+  // console.log("newSchedule:", newSchedule);
+
+  assignedSchedules.forEach((schedule) => {
+    const existingStartTime: any = new Date(
+      `1970-01-10T${schedule.startTime}:00`
+    );
+    const existingEndTime: any = new Date(`1970-01-10T${schedule.endTime}:00`);
+    const newStartTime: any = new Date(
+      `1970-01-10T${newSchedule.startTime}:00`
+    );
+    const newEndTime: any = new Date(`1970-01-10T${newSchedule.endTime}:00`);
+
+    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        `This Facility is not available at that time! Chooes other time or day`
+      );
+    }
+  });
 
   const result = await Booking.create(payload);
   return result;
@@ -14,8 +46,6 @@ const getAllFacilityFromDB = async () => {
   const result = await Booking.find().populate('facility').populate('user');
   return result;
 };
-
-
 
 export const BookingService = {
   createBookingIntoDB,
--- src/app/modules/booking/booking.validation.ts
+++ src/app/modules/booking/booking.validation.ts
@@ -1,24 +1,44 @@
 import { z } from 'zod';
 
 const createBookingValidationSchema = z.object({
-  body: z.object({
-    date: z
-      .string()
-      .regex(
-        /^\d{4}-\d{2}-\d{2}$/,
-        'Invalid date format, should be YYYY-MM-DD'
-      ),
-    startTime: z
-      .string()
-      .regex(/^\d{2}:\d{2}$/, 'Invalid time format, should be HH:MM'),
-    endTime: z
-      .string()
-      .regex(/^\d{2}:\d{2}$/, 'Invalid time format, should be HH:MM'),
-    user: z.string(),
-    facility: z.string(),
-    payableAmount: z.number().nonnegative(),
-    isBooked: z.enum(['confirmed', 'unconfirmed', 'canceled']),
-  }),
+  body: z
+    .object({
+      date: z
+        .string()
+        .regex(
+          /^\d{4}-\d{2}-\d{2}$/,
+          'Invalid date format, should be YYYY-MM-DD'
+        ),
+      startTime: z
+        .string()
+        .regex(/^\d{2}:\d{2}$/, 'Invalid time format, should be HH:MM'),
+      endTime: z
+        .string()
+        .regex(/^\d{2}:\d{2}$/, 'Invalid time format, should be HH:MM'),
+      user: z.string(),
+      facility: z.string(),
+      payableAmount: z.number().nonnegative(),
+      isBooked: z.enum(['confirmed', 'unconfirmed', 'canceled']),
+    })
+    .refine(
+      (body) => {
+        //startTime : 10:30  => 1970-01-10T10:30
+        //endTime : 10:30  => 1970-01-10T12:30
+
+        const start: any = new Date(`1970-01-10T${body.startTime}:00`);
+        const end: any = new Date(`1970-01-10T${body.endTime}:00`);
+        // console.log('start:---', start);
+        // console.log('end:---', end);
+        // const differenceInMilliseconds = end - start;
+        // const differenceInHours = differenceInMilliseconds / 3600000;
+        // console.log('Difference in hours:---', differenceInHours);
+
+        return end > start;
+      },
+      {
+        message: 'Start time should be before End time !',
+      }
+    ),
 });
 
 export const BookingValidation = {
